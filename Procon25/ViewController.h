ca1794bc91000f84c1c93c00a667e168f7a84287
siquare <cubicroonn@gmail.com>
2014-04-03 16:08:25 +0900

    Initial Commit

---
+++ Procon25/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Procon25
+//
+//  Created by Cubic on 2014/04/03.
+//  Copyright (c) 2014年 Cubic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Procon25/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Procon25
+//
+//  Created by Cubic on 2014/04/03.
+//  Copyright (c) 2014年 Cubic. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
